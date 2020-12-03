cb217539548ceae27684d528160e866e7fd0c4c3
Fan Zhang <felicis_zhang@outlook.com>
2020-12-02 21:23:00 -0800

    Updates

---
+++ precache-manifest.1fda5333a29f6db61a0e16dfee69ef60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953bd2e58d44c0c2e1302447c33d2e36",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "f0f3eb7aa57ad99d12c4",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.93d25765.chunk.css"
+  },
+  {
+    "revision": "afae8da9b94203f45434",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.6b163db5.chunk.js"
+  },
+  {
+    "revision": "f0f3eb7aa57ad99d12c4",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.44fb930f.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seainfo6150-final-project-webapp/precache-manifest.247f04989548970dcbda5769a0f254ed.js"
+  "/seainfo6150-final-project-webapp/precache-manifest.1fda5333a29f6db61a0e16dfee69ef60.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/seainfo6150-final-project-webapp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
